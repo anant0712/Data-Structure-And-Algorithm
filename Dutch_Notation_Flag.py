a923f1ebb7e3f8d58f197d013a6244e6eb69fb1f
Anant Mishra <anantmishra0761@gmail.com>
2026-03-27 10:40:49 +0530

    Add files via upload

---
+++ Dutch_Notation_Flag.py
@@ -0,0 +1,18 @@
+def dutch_notation_flag(arr):
+    low = 0
+    mid=0
+    high=len(arr)-1
+
+    while mid<=high:
+        if arr[mid]==0:
+            arr[low],arr[mid]=arr[mid],arr[low]
+            low+=1
+            mid+=1
+        elif arr[mid]==1:
+            mid+=1
+        else:
+            arr[mid],arr[high]=arr[high],arr[mid]
+            high-=1
+    return arr
+
+print(dutch_notation_flag([1,0,2,1,1,0,2]))
