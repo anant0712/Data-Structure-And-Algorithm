dca2513a6414dc76af2f705c55f02d11ae5fc34d
Anant Mishra <anantmishra0761@gmail.com>
2026-07-10 17:22:54 +0530

    Time: 7 ms (80.2%), Space: 19.7 MB (10.09%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -3,15 +3,11 @@ class Solution:
         if n==0:
             return True
         for i in range(len(flowerbed)):
-            if flowerbed[i]==0:
-                left = (i == 0) or (flowerbed[i - 1] == 0)
-                right = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+            if flowerbed[i]==0 and ((i == 0) or (flowerbed[i - 1] == 0)) and ((i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)):
+                flowerbed[i]=1
+                n-=1
 
-                if right and left:
-                    flowerbed[i]=1
-                    n-=1
-
-                    if n==0:
-                        return True
+                if n==0:
+                    return True
         return False
         
