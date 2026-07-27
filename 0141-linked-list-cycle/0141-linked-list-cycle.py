1147c74668d9e02149c45137796da6d82aeb35b5
Anant Mishra <anantmishra0761@gmail.com>
2026-07-27 09:24:45 +0530

    Time: 50 ms (78.48%), Space: 22.8 MB (12.29%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -12,9 +12,7 @@ class Solution:
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-
             if fast == slow:
-                return True
-        
+                return True        
         return False
         
