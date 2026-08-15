8927127dae45b147100cf223c62a725a1023b0c3
Anant Mishra <anantmishra0761@gmail.com>
2026-08-15 17:38:34 +0530

    Time: 5 ms (37.61%), Space: 20.5 MB (36.98%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Since in this question Two sum is mention,hence we get a hint of using Two pointer approach. 
+        #Thus,I created left pointer to point the first element of the array and right pointer to point the last element of an array.
         left = 0
         right = len(numbers)-1
         
+        # We check the condition until left is smaller than right.
         while left<right:
             total = numbers[left] + numbers[right]
 
